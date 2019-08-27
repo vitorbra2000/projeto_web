1af20f577078ed22aa10a101b711fa12ad57ad66
VITOR BRASIL <vitor.brasil@catolicasc.edu.br>
2019-08-26 22:19:03 -0300

    minuto 34:54

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,12 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  var dados = {
+    title: 'aula server side',
+    aluno: 'vitor'
+
+  }
+  res.render('index', dados);
 });
 
 module.exports = router;
