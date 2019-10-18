4b9165c854fc327bda73cc7a3e2dce30d197f840
unknown <vitorbra2000@hotmail.com>
2019-10-18 04:28:03 -0300

    minuto 44

--- routes/admin/posts.js
+++ routes/admin/posts.js
@@ -3,8 +3,17 @@ var router = express.Router();
 var postsService = require('../../services/postsService');
 
 router.get('/', function(req,res,next) {
+    var posts = postsService.getPosts();
 
-    res.render('admin/posts/index');
+    var data = {
+        posts: posts
+    };
+
+    res.render('admin/posts/index', data );
 });
 
+router.get('/create', function(req, res, next){
+
+    res.render('admin/posts/create');
+});
 module.exports = router;
