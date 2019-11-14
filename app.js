f338966673bd879244623e14d6e4b61a3eb64ef5
Vitor Brasil <you@example.com>
2019-11-14 00:41:35 -0300

    1:18:14 msm ainda não foi definido /auth/login

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var session = require('express-session');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -11,6 +12,14 @@ var adminPostsRouter = require('./routes/admin/posts');
 var authRouter =require('./routes/auth');
 
 var app = express();
+
+app.set('trust proxy', 1);
+app.use(session({
+  secret: 'mySecret123',
+  resave: false,
+  saveUninitialized: true,
+  cookie: {secure: false }
+}));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
--- routes/auth.js
+++ routes/auth.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var authService = require('../services/authService');
 
 router.get('/login', function (req, res, next){
 
@@ -11,10 +12,23 @@ router.post('/login', function (req, res, next){
     var email = req.body.email;
     var password = req.body.password;
 
+    if(authService.validateUserEmailAndPassword(email, password)){
+        if(!req.session.authenticadUsers){
+           req.session.authenticadUsers = []; 
+        }
+
+        var user = authService.getUserByEmail(Email);
+    
+        
+        req.session.authenticadUsers.push(user);
+        res.cookie('logintoken', user.loginToken , {maxAge:90000, httpOnly: true });
+
+        req.redirect('/admin/posts');
+    }
     console.log("Email:" + email);
     console.log("senha:" + password);
 
-    req.redirect('/admin/posts');
+    req.render('login', { msg: 'Email e senha incorretos' });
 });
 
 
