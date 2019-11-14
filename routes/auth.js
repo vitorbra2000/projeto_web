6b4764bbe6567b70698acf4b73683d56f554fffa
Vitor Brasil <you@example.com>
2019-11-13 22:25:19 -0300

    minuto 56, assistir ate fazer o if

--- routes/auth.js
+++ routes/auth.js
@@ -5,4 +5,17 @@ router.get('/login', function (req, res, next){
 
     res.render('login', {nsg: ''});
 });
+
+router.post('/login', function (req, res, next){
+
+    var email = req.body.email;
+    var password = req.body.password;
+
+    console.log("Email:" + email);
+    console.log("senha:" + password);
+
+    req.redirect('/admin/posts');
+});
+
+
 module.exports = router;
